4d3adb689ffbf026e807e9c229a5dd762f87cf1c
Martin McNulty <martin.mcnulty@ovoenergy.com>
2018-12-07 16:12:00 +0000

    Pull hosts to monitor from environment variable

--- monitor.py
+++ monitor.py
@@ -1,9 +1,9 @@
 from prometheus_client import Gauge, start_http_server
 import os
 from dateutil import parser
-from datetime import datetime
 import pytz
 import time
+import sys
 
 def get_expiry(host):
     str_exp = os.popen("echo | openssl s_client -showcerts -servername \"%s\" -connect \"%s:443\" 2>/dev/null | openssl x509 -inform pem -noout -dates | grep notAfter | cut -d '=' -f 2" % (host, host)).read().strip()
@@ -15,19 +15,21 @@ def to_timestamp(dt):
     return (dt - epoch).total_seconds()
 
 if __name__ == '__main__':
-    CERT_EXPIRY = Gauge('cert_expiry_datetime', 'SSL certificate expiry date and time (unix seconds)', ['host'])
-    HOSTS = ['***REMOVED***', '***REMOVED***', '***REMOVED***', '***REMOVED***']
+    gauge = Gauge('cert_expiry_datetime', 'SSL certificate expiry date and time (unix seconds)', ['host'])
+    if not 'MONITOR_HOSTS' in os.environ:
+        sys.exit('Required environment variable MONITOR_HOSTS not found')
+    hosts = os.environ['MONITOR_HOSTS'].split(',')
     port = 8000
 
     # Initialize gauge labels
-    for host in HOSTS:
-        CERT_EXPIRY.labels(host)
+    for host in hosts:
+        gauge.labels(host)
 
     start_http_server(port)
     print("Started Prometheus server on %s" % port)
 
     while True:
-        print("Updating expiry dates for %s..." % HOSTS)
-        for host in HOSTS:
-            CERT_EXPIRY.labels(host).set(to_timestamp(get_expiry(host)))
+        print("Updating expiry dates for %s..." % hosts)
+        for host in hosts:
+            gauge.labels(host).set(to_timestamp(get_expiry(host)))
         time.sleep(60)
